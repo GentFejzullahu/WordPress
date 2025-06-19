e10d75e3b80cc8a7b65549857f0c1f1dd5094988
GentFejzullahu <gentfejzullahu576@gmail.com>
2025-06-19 07:05:35 -0700

    save

---
+++ front-page.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+<div id="container">
+    <div id="inner_container">
+        <div class="img_container">
+            <div class="img_container">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/FigojSAXkA0Cjm0.jpg" alt="Image 1" />
+            </div>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/62739920-emir-sahiti.jpg" alt="Image 2" />
+        </div>
+         </div>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/OIP.jpg" alt="Image 2" />
+        </div>
+    </div>
+</div>
